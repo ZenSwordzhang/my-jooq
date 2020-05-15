ff97ca027e162cf26598dc4f90a8efeed2d0ec18
zsx <18273117003@163.com>
2020-05-15 17:24:47 +0800

    udp

---
+++ my-python/udp_client.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+import socket  # 网络通信 TCP，UDP
+
+# SOCK_DGRAM表示UDP，SOCKET_STREAM表示TCP
+client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+while True:
+    msg = input('>>: ').strip()
+    if not msg:
+        continue
+
+    client.sendto(msg.encode('utf-8'), ('192.168.1.110', 8080))
+
+    back_msg, addr = client.recvfrom(1024)
+    print(back_msg.decode('utf-8'), addr)
+    if back_msg.decode('utf-8') == 'CLOSE':
+        break
+client.close()
---
+++ my-python/udp_server.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+import socket
+
+# 基于网络的数据报协议 UDP
+server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+# 绑定这个端口，接收这个端口的消息，参数是元组，括号不能少
+server.bind(('192.168.1.110', 8080))
+
+while True:
+    # 1024 缓冲区。recvfrom可以得到发送方的消息和地址，recv只能得到消息
+    msg, addr = server.recvfrom(1024)
+    print(msg, addr)
+    # 发送数据到指定的地址
+    server.sendto(msg.upper(), addr)
