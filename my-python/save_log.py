99585dbb9f4209e252fd7cdf195b3dac2be14e01
zsx <18273117003@163.com>
2020-05-15 13:40:29 +0800

    python generate log file

--- my-python/save_log.py
+++ my-python/save_log.py
@@ -9,9 +9,9 @@ import os
 path = "D:/usr/local/logs"
 if not os.path.exists(path):
     os.makedirs(path)
-
+today = datetime.date.today()
 # 方法1： 此方法文件夹不存在会报错：FileNotFoundError: [Errno 2] No such file or directory
-with open('D:/usr/local/logs/spring.log', mode='a+', encoding='utf-8') as f:
+with open(f'D:/usr/local/logs/spring-{today}.log', mode='a+', encoding='utf-8') as f:
     now = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f')[:-3]
     thread_id = threading.currentThread().native_id
     log_str = f'{now} DEBUG {thread_id} --- [scheduling-1] org.jooq.tools.LoggerListener            : ' \
