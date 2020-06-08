8e3ac36e06752a5a5f656d4598248c17b1e7c2b6
zsx <18273117003@163.com>
2020-06-08 11:15:55 +0800

    ELK config

--- docs/shell/wsl.sh
+++ docs/shell/wsl.sh
@@ -8,13 +8,16 @@ sudo service ssh start
 sudo docker restart postgres
 
 # 递归方式给新生成的日志文件设置权限
-sudo chomod -R 777 /data/dmplus/facilities/log
+sudo chmod -R 777 /data/dmplus/facilities/log
 
 sudo chmod 777 /etc/sysctl.conf
 # 输出内容到文件
 sudo echo vm.max_map_count=262144 >> /etc/sysctl.conf
 # 恢复原权限
 sudo chmod 644 /etc/sysctl.conf
+
+# 重新加载配置
+sudo sysctl --system
 
 
 sudo docker restart elasticsearch logstash filebeat kibana
