fca598feaf8fb329fbee27dd0aff660d3f0b0f24
zsx <18273117003@163.com>
2020-06-11 13:36:07 +0800

    redis create configuration

--- docs/docker/test.sh
+++ docs/docker/test.sh
@@ -1,30 +1,32 @@
 #!/usr/bin/bash
 
-container_names=
-# 生成要检查的容器名
-for port in $(seq 7000 7005); do
-  container_names=$container_names"|redis-${port}"
-done
-
-#echo "container_names: $container_names"
-
-# 去掉字符串首位的"|"，得到grep命令的匹配部分
-grep_part="${container_names:1}"
-#echo "grep_part: $grep_part"
-
-## 拼接命令实现过滤，匹配容器信息，判断容器是否存在，如果容器不存在，直接创建
-info_list=$(docker ps -a | grep -E "$grep_part" | tee redis.txt)
-if test -z "$info_list"; then
-  exit
-fi
-# 按2个以上空格进行全局替换
-< redis.txt sed 's/[[:space:]]\{2,\}/=/g' > tmpfile && mv tmpfile redis-sed.txt
-# 按"="好进行切分，获取匹配的容器名
-container_names_list=$(cut -d "=" -f7 redis-sed.txt)
-echo "$container_names_list already exists, delete or not? y/n:"
-read -r deleted_str
-if [ "$deleted_str" == "y" ]; then
-  remove_redis_cluster="docker rm -f""${container_names//|/' '}"
-  $remove_redis_cluster
-  docker ps -a
-fi
+createRedisConfig() {
+  echo "Start writing to redis configuration file"
+  for port in $(seq 7000 7005); do
+    if [ ! -d "d:/usr/local/etc/docker/redis-cluster/config/${port}" ]; then
+      mkdir -p d:/usr/local/etc/docker/redis-cluster/config/"${port}"
+    fi
+    # 使用"<<-"，需要使用制表符Tab缩进
+    # 使用<<-'EOF'或<<'EOF'，其中的内容都不能进行变量替换，使用<<EOF可以将外部变量传递到内容中
+    tee d:/usr/local/etc/docker/redis-cluster/config/"${port}"/redis.conf <<EOF
+# redis后台运行
+daemonize no
+# redis运行的端口号
+port $port
+# 指定只接收来自该ip地址的请求
+bind 0.0.0.0
+# 启动集群模式
+cluster-enabled yes
+# 集群配置文件，在集群启动时，自动创建
+cluster-config-file nodes-$port.conf
+# 节点总线端口
+cluster-announce-bus-port 1$port
+# 集群超时时间，节点超时多久表示宕机了
+cluster-node-timeout 50000
+# 开启aof持久化模式，每次写操作请求都追加到appendonly.aof文件中
+appendonly yes
+EOF
+  done
+  echo "Writing redis configuration file is complete"
+}
+createRedisConfig
